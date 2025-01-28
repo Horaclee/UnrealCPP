eaf03062219c49fa413a7fe6e7dbe5dca306de19
Horaclee <115532821+Horaclee@users.noreply.github.com>
2025-01-28 15:46:46 +0100

    Delete Source directory

--- Source/GTECH_A/GTECH_A.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GTECH_A.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GTECH_A, "GTECH_A" );
